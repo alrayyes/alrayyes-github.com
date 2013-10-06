6471423b0ef385d60aabfec52f30f52bb63318ed
Ryan <alrayyes@gmail.com>
2013-10-07 01:14:50 +0200

    Modified stylesheets & javascripts

--- source/javascripts/bottom.js
+++ source/javascripts/bottom.js
@@ -1,4 +1,3 @@
 //= require "vendor/jquery-1.10.1.min"
-//= require "vendor/bootstrap.min"
 //= require "plugins"
 //= require "main"
--- source/javascripts/top.js
+++
@@ -1,1 +0,0 @@
-//= require "vendor/modernizr-2.6.2-respond-1.1.0.min.js"
